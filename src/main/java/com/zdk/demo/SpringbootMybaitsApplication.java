0a41113d0593f563aae0eb2fa946cf5b1590ff45
TomismeZ <1840317559@qq.com>
2018-06-15 15:33:16 +0800

    更新

--- src/main/java/com/zdk/demo/SpringbootMybaitsApplication.java
+++ src/main/java/com/zdk/demo/SpringbootMybaitsApplication.java
@@ -3,12 +3,14 @@ package com.zdk.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.zdk.demo.mapper")
 @EnableSwagger2
+@EnableTransactionManagement // 启注解事务管理，等同于xml配置方式的 <tx:annotation-driven />  
 public class SpringbootMybaitsApplication {
 
 	public static void main(String[] args) {
